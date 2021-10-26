d67045c40b738df164b5d994f799d9302b20b704
Kbaths <kbath.dev@gmail.com>
2021-10-26 01:52:09 -0300

    Styling social media

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { DiCssdeck } from 'react-icons/di';
 
-import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
+import { Container, Span, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
 const Header = () =>  (
   <Container>
     <Div1>
       <Link href="/">
-        <a style={{ display: "flex", alignItems: "center", color: "white" }}>
-          <DiCssdeck size="3em" /> <span>Portifolio</span>
+        <a style={{ display: "flex", alignItems: "center", color: "white", marginTop: "-15px" }}>
+          <DiCssdeck size="3em" /> <Span>Portifolio</Span>
         </a>
       </Link>
     </Div1>
@@ -32,6 +32,17 @@ const Header = () =>  (
         </Link>  
       </li> 
     </Div2>
+    <Div3>
+      <SocialIcons href='https://github.com'> 
+        <AiFillGithub size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href='https://linkedin.com'> 
+        <AiFillLinkedin size="3rem"/>
+      </SocialIcons>
+      <SocialIcons href='https://instagram.com'> 
+        <AiFillInstagram size="3rem"/>
+      </SocialIcons>
+    </Div3>
   </Container>
 );
 
