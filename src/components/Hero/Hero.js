9e6994661e82fb9004052b9b503db60f3dbc6901
Kbaths <kbath.dev@gmail.com>
2021-10-26 02:11:50 -0300

    creating the hero session

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Bem Vindo ao <br/>
+        Meu Portifolio pessoal
+      </SectionTitle>
+      <SectionText>
+        Ainda vou bolar algo para colocar aqui
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'}>Entre em contato</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
